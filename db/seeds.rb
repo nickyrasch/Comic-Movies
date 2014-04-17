933caf6c49406598dac372427ae7bb1c58f59916
Mike <info@nickyrasch.com>
2014-04-17 16:26:09 -0700

    Add CSS styles to movie list page [Finishes #69668114]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,12 @@
 upcoming = Type.new(name: 'upcoming')
 archives = Type.new(name: 'archives')
 
-amazing_spider_man_2 = Movie.new(title: 'Amazing Spider-Man_2',
+amazing_spider_man_2 = Movie.new(title: 'Amazing Spider-Man 2',
              image_file_name: 'amazing_spider-man_2')
 x_men_4 = Movie.new(title: 'X-Men Days of Future Past',
              image_file_name: 'x-men_4')
 guardians = Movie.new(title: 'Guardians of the Galaxy',
              image_file_name: 'guardians')
-avengers_2 = Movie.new(title: 'Avengers:Age of Ultron',
-             image_file_name: 'avengers_2')
 spider_man_1 = Movie.new(title: 'Spider-Man 1',
              image_file_name: 'spider-man_1')
 spider_man_2 = Movie.new(title: 'Spider-Man 2',
@@ -19,16 +17,13 @@ spider_man_3 = Movie.new(title: 'Spider-Man 3',
 amazing_spider_man_2.type = upcoming
 x_men_4.type = upcoming
 guardians.type = upcoming
-avengers_2.type = upcoming
 spider_man_1.type = archives
 spider_man_2.type = archives
 spider_man_3.type = archives
 
-avengers_2.save
 guardians.save
 x_men_4.save
 amazing_spider_man_2.save
-
 spider_man_3.save
 spider_man_2.save
 spider_man_1.save
